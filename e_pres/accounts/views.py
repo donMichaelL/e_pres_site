f5f40ce8ceec8a5afbf6b4a51789955da7d1bc7d
Michael <michael.loukeris@icloud.com>
2016-01-07 13:08:54 +0200

    Home Page visitor

---
+++ e_pres/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import View
+from allauth.account.forms import LoginForm
+# Create your views here.
+
+class HomepageView(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            print 'Hello'
+        else:
+            form = LoginForm()
+            context = {'form': form}
+            return render(request, 'accounts/homepage_visitor.html', context)
