54d7fbfc61bac6b49f1fe213a04ed23d4cb65c57
Michael <michael.loukeris@icloud.com>
2016-02-18 14:44:07 +0200

    Model for checkpoint fail plan

--- e_pres/analytics/models.py
+++ e_pres/analytics/models.py
@@ -1,13 +1,23 @@
 from __future__ import unicode_literals
 from django.db import models
 from experiments.models import Experiment, Checkpoint
-
+from plans.models import Plan
 
 class CheckpointReport(models.Model):
     experiment = models.ForeignKey(Experiment)
     checkpoint = models.ForeignKey(Checkpoint)
     current_flux = models.PositiveSmallIntegerField(null=True, blank=True)
     fail = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.experiment.name
+
+
+class CheckpointFailPlan(models.Model):
+    experiment = models.ForeignKey(Experiment)
+    plan = models.ForeignKey(Plan)
+    last_current_checkpoint = models.ForeignKey(Checkpoint)
+    tag = models.IntegerField()
 
     def __unicode__(self):
         return self.experiment.name
