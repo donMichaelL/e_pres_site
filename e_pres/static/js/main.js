f9b30d3004be2d58b6c564b40761e1d4e00c3b14
Michael <michael.loukeris@icloud.com>
2016-03-15 17:15:17 +0200

    We add delete checkpoint as ajax request

--- e_pres/static/js/main.js
+++ e_pres/static/js/main.js
@@ -9,3 +9,20 @@ var $grid = $('.grid').imagesLoaded( function() {
 $(".alert-success").fadeTo(2000, 500).slideUp(500, function(){
     $(".alert-success").alert('close');
 });
+
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
