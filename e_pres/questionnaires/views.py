01b70c821afa55af103bc3790995fb4397dd20cf
Michael <michael.loukeris@icloud.com>
2016-05-23 17:55:01 +0300

    Styling form questionnaire

--- e_pres/questionnaires/views.py
+++ e_pres/questionnaires/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from buildings.models import Building
 from .models import PreparednessQuestionnaireQuestion
 
 
-class PreparednessQuestionnaireView(ListView):
-    model = PreparednessQuestionnaireQuestion
+class PreparednessQuestionnaireView(DetailView):
+    model = Building
     template_name = 'dashboard/questionnaires/list_preparednessQuestions.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PreparednessQuestionnaireView, self).get_context_data(**kwargs)
+        context['preparedness_questions'] = PreparednessQuestionnaireQuestion.objects.all()
+        return context
