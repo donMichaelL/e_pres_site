fded130edd003eef1eefed9def9f87d9af400743
Michael <michael.loukeris@icloud.com>
2016-06-24 15:46:04 +0300

    Creating config file trials.py

---
+++ e_pres/e_pres/config/trials.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': get_secret('PRODUCTION_DATABASE_USER'),
+        'PASSWORD': get_secret('PRODUCTION_DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
