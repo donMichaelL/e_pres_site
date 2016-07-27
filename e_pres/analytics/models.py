c2e4982ce7037d6bc5a6cd54c7c91dd8477478be
Michael <michael.loukeris@icloud.com>
2016-07-27 12:41:27 +0300

    adding field error code in checkpoint fail plan

--- e_pres/analytics/models.py
+++ e_pres/analytics/models.py
@@ -15,12 +15,18 @@ class CheckpointReport(models.Model):
     def __unicode__(self):
         return self.experiment.name
 
+ERROR_CODE = (
+    ('1', 'Correct Execution'),
+    ('2', 'Path Before Path'),
+    ('3', 'Teacher'),
+)
 
 class CheckpointFailPlan(models.Model):
     experiment = models.ForeignKey(Experiment)
     tag_r = models.ForeignKey(Tag)
     plan = models.ForeignKey(Plan)
     last_current_checkpoint = models.ForeignKey(Checkpoint, null=True, blank=True)
+    error_code = models.CharField(max_length=2, choices=ERROR_CODE)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
     def __unicode__(self):
